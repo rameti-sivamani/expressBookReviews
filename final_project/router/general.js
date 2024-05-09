c1a58a9843d19c1f4bd70a4dc162de30e8854e4e
rameti sivamani <sivamanirameti494@gmail.com>
2024-05-09 14:01:57 +0530

    change the updates

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,18 +13,22 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN=req.params.isbn;
+  return res.send(books[ISBN]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+  const author=req.params.author;
+  const booksByAuthor=Object.values(books).filter(book=>book.author===author)
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
